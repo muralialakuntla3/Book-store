008d26d39defb3cea470727a3e19557eeb724c5d
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-12-22 17:16:26 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -5,38 +5,48 @@ function Createdb(){
     $username = "user";
     $password = "test";
     $dbname = "bookstore";
+    $port = 3306;
 
-    // create connection
-    $conn = mysqli_connect($servername, $username, $password);
+    // Create connection
+    $con = mysqli_connect($servername, $username, $password, '', $port);
 
     // Check Connection
     if (!$con){
         die("Connection Failed : " . mysqli_connect_error());
     }
 
-    // create Database
+    // Create Database
     $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
 
     if(mysqli_query($con, $sql)){
-        $con = mysqli_connect($servername, $username, $password, $dbname);
+        $con = mysqli_connect($servername, $username, $password, $dbname, $port);
 
         $sql = "
-                        CREATE TABLE IF NOT EXISTS books(
-                            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            book_name VARCHAR (25) NOT NULL,
-                            book_publisher VARCHAR (20),
-                            book_price FLOAT 
-                        );
+            CREATE TABLE IF NOT EXISTS books(
+                id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                book_name VARCHAR(25) NOT NULL,
+                book_publisher VARCHAR(20),
+                book_price FLOAT
+            );
         ";
 
         if(mysqli_query($con, $sql)){
             return $con;
-        }else{
+        } else {
             echo "Cannot Create table...!";
         }
-
-    }else{
+    } else {
         echo "Error while creating database ". mysqli_error($con);
     }
-
 }
+
+// Create the database and table if they don't exist
+$database = Createdb();
+
+// Check if the connection is established
+if ($database) {
+    echo "Database and Table created successfully";
+} else {
+    echo "Database creation failed";
+}
+?>
